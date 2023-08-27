3058ba3faa730b7a620ce691d6f35ddfa6eef5ec
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-27 16:13:05 -0700

    server file edits

--- server/server.js
+++ server/server.js
@@ -35,7 +35,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.applyMiddleware({ app });
 
   db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 })
 };
 
