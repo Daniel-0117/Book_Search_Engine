f0093fe7f87f13994ca38c4190bd5a350de73cee
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-27 21:15:56 -0700

    App page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Apolloclient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new Apolloclient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -20,11 +31,12 @@ function App() {
           />
           <Route 
             path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
+            element={<h1 className='display-2'>You have taken the wrong path Ashen One</h1>}
           />
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
