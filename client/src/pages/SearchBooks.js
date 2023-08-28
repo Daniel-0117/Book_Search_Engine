4ce36c0f9db79d7db5bd80837f8a5d20940d7728
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-27 22:00:33 -0700

    added gitignores

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -63,7 +63,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
